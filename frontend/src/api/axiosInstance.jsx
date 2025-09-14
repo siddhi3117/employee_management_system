f9fa414eea271872b2d4e908af6b2cc958ecd2c1
Arman-Shaikh58 <armanshaikh236058@gmail.com>
2025-09-14 19:41:31 +0530

    Added Frontend and backend URLs in env file

--- frontend/src/api/axiosInstance.jsx
+++ frontend/src/api/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
-
+const BASEURL= import.meta.env.VITE_BACKEND_URL;
 const api=axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:BASEURL,
   headers: {
     "Content-Type": "application/json",
   },
